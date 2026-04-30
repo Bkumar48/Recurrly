829d62d59f65380c5a488207d06253079334cd4a
Bkumar48 <bittukumarinfo48@gmail.com>
2026-04-30 23:54:48 +0530

    chore: prebuild setup

--- app.config.js
+++ app.config.js
@@ -1,12 +1,15 @@
-const appJson = require('./app.json')
+const appJson = require("./app.json");
 
 export default {
   expo: {
     ...appJson.expo,
+    android: {
+      package: "com.bittukumar.recurrly",
+    },
     extra: {
       ...(appJson.expo?.extra || {}),
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
     },
   },
-}
+};
